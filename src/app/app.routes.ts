287314edf3fe2d4693f979b25c6bbf1735a0beef
Dhanyashree <dhanyakarkera100@gmail.com>
2024-10-10 14:54:58 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { Task1Component } from './task-1/task-1.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:Task1Component 
+    }
+    // {
+    //      path: '', redirectTo: 'task-1', pathMatch: 'full'
+    // }
+];
